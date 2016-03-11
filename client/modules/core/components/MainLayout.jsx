097b1705c778470e11f3f694025a02adb08c3f11
Jonathan Poltak Samosir <jonathan.samosir@gmail.com>
2016-03-11 11:48:44 +0700

    Updated MainLayout to use react-bootstrap components

--- client/modules/core/components/MainLayout.jsx
+++ client/modules/core/components/MainLayout.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import {Grid, Row, Col, PageHeader} from 'react-bootstrap';
 
 const Layout = ({content = () => null}) => (
-  <div>
+  <Grid>
+    <Row>
+      <Col xs={6} md={8} xsOffset={3} mdOffset={4}>
+        <PageHeader>
+          Impossible List
+        </PageHeader>
+      </Col>
+    </Row>
 
-    <header>
-    	<h1>Impossible List</h1>
-    </header>
-
-    <div>
+    <Row>
     	{content()}
-    </div>
-
-  </div>
+    </Row>
+  </Grid>
 );
 
 export default Layout;
